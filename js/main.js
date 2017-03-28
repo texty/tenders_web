c22ae1352c0bb8140017264009417347154e4423
Vladislav <drimacus182@gmail.com>
2017-03-28 16:45:37 +0300

    Бій з дататейблз, чекпоінт 2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 $(document).ready(function() {
+    var currencyFmt = function(d){
+        return d3.format(",.0f")(+d / 1000).replace(/,/g, " ");
+    };
+
+
+
     d3.csv('data/data1.csv', function(err, data1){
         if (err) throw err;
 
+        data1.forEach(function(d){d.total_volume = +d.total_volume});
+
         // debugger;
-        // return;
+
         $('#scheme-one-table').DataTable({
-            "aaData": data1,
-            "aoColumns": [
-                {"mData": "winner_name", sTitle: "Переможець"},
-                {"mData": "loser_name", sTitle: "Лузер"},
-                {"mData": "total_count", sTitle: "Брали участь у тендерах"},
-                {"mData": "total_volume", sTitle: "Сума по всіх тендерах"}
-            ]
+            language: {url: "Ukrainian.json"},
+
+            aaData: data1,
+            aoColumns: [
+                {mData: "winner_name", sTitle: "Переможець"},
+                {mData: "loser_name", sTitle: "Лузер"},
+                {mData: "total_count", sTitle: "Брали участь у тендерах"},
+                {mData: "total_volume", mRender: function(data, type, full) {
+                    if (type === 'display') return currencyFmt(full.total_volume);
+                    return data
+                }, sTitle: "Сума по всіх тендерах, тис. грн"}
+                // {mData: "total_volume", sTitle: "Сума по всіх тендерах"}
+            ],
+
+            order: [[3, "desc"]]
         });
     });
 
